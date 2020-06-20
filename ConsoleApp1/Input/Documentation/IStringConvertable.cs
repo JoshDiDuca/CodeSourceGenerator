3577e49cb581870f6629b9572b4867425c528ff6
Duke <josh@di-duca.co.uk>
2020-06-20 14:23:26 +0100

    Cleaned up documentation example

--- ConsoleApp1/Input/Documentation/IStringConvertable.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ConsoleApp1.Input.Typescript
-{
-    /// <summary>
-    /// Marker interface for collections that can render themselves as string
-    /// </summary>
-    public interface IStringConvertable
-    {
-    }
-}
